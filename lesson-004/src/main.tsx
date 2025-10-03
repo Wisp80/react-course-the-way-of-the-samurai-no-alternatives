d697945dfa0c6faeed6aa30dc34b8bbe9232c4a9
Wisp <therearewispseverywhere@gmail.com>
2025-10-03 22:37:16 +0300

    lesson 004 add more info and complete homework

--- lesson-004/src/main.tsx
+++ lesson-004/src/main.tsx
@@ -1,7 +1,8 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import {createRoot} from 'react-dom/client';
+import './index.css';
+import App from './App.tsx';
+// import Homework004 from './App.tsx';
 
-const rootEl = document.getElementById('root')
-const reactRoot = createRoot(rootEl!)
-reactRoot.render(<App />)
+const rootEl = document.getElementById('root');
+const reactRoot = createRoot(rootEl!);
+reactRoot.render(<App/>);
