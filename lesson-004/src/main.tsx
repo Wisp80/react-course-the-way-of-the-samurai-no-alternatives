c3d7e7a9b5367c2732b69bdb57badd5c6ab92123
Wisp <therearewispseverywhere@gmail.com>
2025-10-05 22:35:11 +0300

    lesson 004 fix imports

--- lesson-004/src/main.tsx
+++ lesson-004/src/main.tsx
@@ -1,8 +1,14 @@
 import {createRoot} from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
-// import Homework004 from './App.tsx';
+import {Homework004} from './App.tsx';
 
 const rootEl = document.getElementById('root');
 const reactRoot = createRoot(rootEl!);
-reactRoot.render(<App/>);
+
+reactRoot.render(
+    <>
+        <App/>
+        <Homework004/>
+    </>
+);
