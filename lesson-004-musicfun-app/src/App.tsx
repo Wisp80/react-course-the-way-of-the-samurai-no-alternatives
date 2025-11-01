abef7712b16e42310e1c953903b24ed8e07355f0
Wisp <therearewispseverywhere@gmail.com>
2025-11-01 21:59:39 +0300

    lesson 013 UX-UI pattern List-Detail, lesson 004 add request for more details on tracks, lesson 008 add request for more details on tasks

--- lesson-004-musicfun-app/src/App.tsx
+++ lesson-004-musicfun-app/src/App.tsx
@@ -1,8 +1,9 @@
 import {useEffect, useState} from 'react';
 
 export function App() {
-    const [selectedTrackId, setSelectedTrackId] = useState(1);
-    const [tracks, setTracks] = useState(null)
+    const [selectedTrackId, setSelectedTrackId] = useState(null);
+    const [selectedTrack, setSelectedTrack] = useState(null);
+    const [tracks, setTracks] = useState(null);
 
     useEffect(() => {
         console.log('effect');
@@ -11,50 +12,81 @@ export function App() {
                 'api-key': '9eed344f-5a69-4e3b-92dc-6f0f7f443157'
             }
         }).then(res => res.json())
-            .then(json => setTracks(json.data))
-    }, [])
+            .then(json => setTracks(json.data));
+    }, []);
 
     if (tracks === null) {
         return <div>
             <h1>Musicfun Player</h1>
             <span>loading...</span>
-        </div>
+        </div>;
     }
 
     if (tracks.length === 0) {
         return <div>
             <h1>Musicfun Player</h1>
             <span>No tracks</span>
-        </div>
-    };
+        </div>;
+    }
 
     return (
         <div>
             <h1>Musicfun Player</h1>
-            <button onClick={ () => {
-                setSelectedTrackId(null)
-            } }>reset selection</button>
+            <button onClick={() => {
+                setSelectedTrackId(null);
+                setSelectedTrack(null);
+            }}>reset selection
+            </button>
 
-            <ul>
-                {
-                    tracks.map(
-                        (track) => {
-                            return (
-                                <li key={track.id} style={ {
-                                    border: track.id === selectedTrackId ? '1px solid darkorange' : 'none'
-                                } }>
-                                    <div onClick={ () => {
-                                        setSelectedTrackId(track.id);
-                                    } }>
-                                        {track.attributes.title}
-                                    </div>
-                                    <audio src={track.attributes.attachments[0].url} controls></audio>
-                                </li>
-                            )
-                        }
-                    )
-                }
-            </ul>
+            <div style={{
+                display: 'flex',
+                gap: '30px'
+            }}>
+                <ul>
+                    {
+                        tracks.map(
+                            (track) => {
+                                return (
+                                    <li key={track.id} style={{
+                                        border: track.id === selectedTrackId ? '1px solid darkorange' : 'none'
+                                    }}>
+                                        <div onClick={() => {
+                                            setSelectedTrackId(track.id);
+
+                                            fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks/' + track.id, {
+                                                headers: {
+                                                    'api-key': '9eed344f-5a69-4e3b-92dc-6f0f7f443157'
+                                                }
+                                            }).then(res => res.json())
+                                                .then(json => setSelectedTrack(json.data));
+                                        }}>
+                                            {track.attributes.title}
+                                        </div>
+                                        <audio src={track.attributes.attachments[0].url} controls></audio>
+                                    </li>
+                                );
+                            }
+                        )
+                    }
+                </ul>
+
+                <div>
+                    <h2>Details</h2>
+                    {
+                        selectedTrackId && (!selectedTrack || selectedTrack.id !== selectedTrackId)
+                            ? 'loading...'
+                            : selectedTrack === null
+                                ? 'Track is not selected'
+                                : <div>
+                                    <h3>{selectedTrack.attributes.title}</h3>
+                                    <h4>Lyrics</h4>
+                                    <p>
+                                        {selectedTrack.attributes.lyrics ?? 'no lyrics'}
+                                    </p>
+                                </div>
+                    }
+                </div>
+            </div>
         </div>
     );
-};
+}
--- lesson-008-task-manager-app/src/App.tsx
+++ lesson-008-task-manager-app/src/App.tsx
@@ -1,7 +1,7 @@
 import {useEffect, useState} from 'react';
 
 export function App() {
-    const [tasks, setTasks] = useState(null)
+    const [tasks, setTasks] = useState(null);
 
     useEffect(() => {
         fetch('https://trelly.it-incubator.app/api/1.0/boards/tasks', {
@@ -9,10 +9,11 @@ export function App() {
                 'api-key': '34ba8abb-f37c-46d9-9a4f-8f6b36b1b225'
             }
         }).then(res => res.json())
-            .then(json => setTasks(json.data))
-    }, [])
+            .then(json => setTasks(json.data));
+    }, []);
 
-    const [highlightedTaskId, setHighlightedTaskId] = useState(null);
+    const [selectedTaskId, setSelectedTaskId] = useState(null);
+    const [selectedTask, setSelectedTask] = useState(null);
 
     if (tasks === null)
         return <h1>Загрузка...</h1>;
@@ -22,50 +23,91 @@ export function App() {
 
     return <div>
         <button onClick={() => {
-            setHighlightedTaskId(null);
+            setSelectedTaskId(null);
+            setSelectedTask(null);
         }}>Сбросить выделение
         </button>
 
-        {tasks.map((task, index) => {
-            return (
-                <div key={index} style={{
-                    border: task.id === highlightedTaskId ? '8px solid blue' : '8px solid black',
-                    marginBottom: '10px',
-                    width: '30%',
-                    backgroundColor:
-                        task.attributes.priority === 0 ? '#ffffff' :
-                            task.attributes.priority === 1 ? '#ffd7b5' :
-                                task.attributes.priority === 2 ? '#ffb38a' :
-                                    task.attributes.priority === 3 ? '#ff9248' :
-                                        task.attributes.priority === 4 ? '#ff6700' : 'white'
-                }}>
+        <div style={{
+            display: 'flex',
+            gap: '44px',
+        }}>
+            <div>
+                {tasks.map((task, index) => {
+                    return (
+                        <div key={index} style={{
+                            border: task.id === selectedTaskId ? '8px solid blue' : '8px solid black',
+                            marginBottom: '10px',
+                            width: '100%',
+                            backgroundColor:
+                                task.attributes.priority === 0 ? '#ffffff' :
+                                    task.attributes.priority === 1 ? '#ffd7b5' :
+                                        task.attributes.priority === 2 ? '#ffb38a' :
+                                            task.attributes.priority === 3 ? '#ff9248' :
+                                                task.attributes.priority === 4 ? '#ff6700' : 'white'
+                        }}>
 
-                    <div style={{
-                        marginLeft: '10px',
-                        fontSize: '20px'
-                    }}
+                            <div style={{
+                                marginLeft: '10px',
+                                fontSize: '20px'
+                            }}
 
-                         onClick={() => {
-                             setHighlightedTaskId(task.id);
-                         }}>
-                        <p style={{
-                            textDecoration: task.attributes.status === 2 ? 'line-through' : 'none'
-                        }}
-                        ><strong>Заголовок:</strong> {task.attributes.title} </p>
+                                 onClick={() => {
+                                     setSelectedTaskId(task.id);
 
-                        <div> Статус:
-                            <input
-                                type="checkbox"
-                                defaultChecked={task.attributes.status === 2}
-                            />
+                                     fetch('https://trelly.it-incubator.app/api/1.0/boards/' + task.attributes.boardId + '/tasks/' + task.id, {
+                                         headers: {
+                                             'api-key': '34ba8abb-f37c-46d9-9a4f-8f6b36b1b225'
+                                         }
+                                     }).then(res => res.json())
+                                         .then(json => setSelectedTask(json.data));
+                                 }}>
+                                <p style={{
+                                    textDecoration: task.attributes.status === 2 ? 'line-through' : 'none'
+                                }}
+                                ><strong>Заголовок:</strong> {task.attributes.title} </p>
+
+                                <div> Статус:
+                                    <input
+                                        type="checkbox"
+                                        defaultChecked={task.attributes.status === 2}
+                                    />
+                                </div>
+
+                                <p>
+                                    <b>Дата создания
+                                        задачи</b>: {new Date(task.attributes.addedAt).toLocaleDateString()}
+                                </p>
+                            </div>
                         </div>
+                    );
+                })}
+            </div>
 
-                        <p>
-                            <b>Дата создания задачи</b>: {new Date(task.attributes.addedAt).toLocaleDateString()}
-                        </p>
-                    </div>
+            <div
+                style={{
+                    border: '8px solid black',
+                    height: '497px',
+                    width: '20%'
+                }}>
+                <div style={{
+                    marginLeft: '15px',
+                }}>
+                    <h2>Task details</h2>
+                    <div>{
+                        selectedTaskId && (!selectedTask || selectedTask.id !== selectedTaskId)
+                            ? 'loading...'
+                            : selectedTask === null
+                                ? 'Task is not selected'
+                                : <div>
+                                    <p>title - {selectedTask.attributes.title  ?? 'no title'}</p>
+                                    <p>boardTitle - {selectedTask.attributes.boardTitle ?? 'no boardTitle'}</p>
+                                    <p>description - {selectedTask.attributes.description ?? 'no description'}</p>
+                                </div>
+                    }</div>
                 </div>
-            );
-        })}
-</div>
+            </div>
+
+        </div>
+    </div>;
 }
